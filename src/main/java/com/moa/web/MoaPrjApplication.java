6c7a1a07b980a1e82bdc3a7d84521d18e8dac520
donggi <ldg6153@gmail.com>
2020-07-20 15:04:04 +0900

    git 햣햣

--- src/main/java/com/moa/web/MoaPrjApplication.java
+++ src/main/java/com/moa/web/MoaPrjApplication.java
@@ -10,6 +10,9 @@ public class MoaPrjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoaPrjApplication.class, args);
+		System.out.println("avg");
+		//버전1 추가
+		//깃 리버트
 	}
 
 }
--- src/main/resources/static/js/board/introlook/list.js
+++ src/main/resources/static/js/board/introlook/list.js
@@ -1,11 +1,11 @@
 $(function(){
+	/*
 	let detail = $(".detail");
 	let img = $(".thumbnail-img");
 	
 	img.click(function (){
 		detail.toggle(200);
 	});
-	
 	
     detail.click(function () {
         detail.toggle(500);
@@ -16,6 +16,10 @@ $(function(){
     times.click(function () {
         detail.toggle(500);
     });
+	*/
 	
+	let detail = $(".board-container:nth-child()");
+	let casual = $("img[name="casual"]");
 	
+	console.log(casual);
 });
